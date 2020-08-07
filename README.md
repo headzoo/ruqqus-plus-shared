904847c16b98f72deb00a50471c37cf91fbc6d9e
headzoo <sean@headzoo.io>
2020-08-07 11:55:30 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Shared library for ruqqus-plus browser extension
---
+++ themes.js
@@ -0,0 +1,31 @@
+/**
+ * @param {{}} theme
+ * @returns {string|boolean}
+ */
+const validateThemeJSON = (theme) => {
+    if (!theme.uuid) {
+        return 'Missing UUID';
+    }
+    if (!theme.name || theme.name.trim() === '') {
+        return 'Theme must have a name.';
+    }
+    if (!theme.version || theme.version.trim() === '') {
+        return 'Theme must have a version';
+    }
+    const match = theme.version.match(/^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/);
+    if (!match) {
+        return 'Version must match x.x.x for example 1.33.5';
+    }
+    if (!theme.author || theme.author.trim() === '') {
+        return 'Theme must have an author';
+    }
+    if (theme.website) {
+        try {
+            new URL(theme.website); // eslint-disable-line
+        } catch (error) {
+            return 'Invalid website URL.';
+        }
+    }
+
+    return false;
+};
